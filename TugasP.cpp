2c222a875ed012c0ebb268bcf067ee3deca75193
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2024-05-30 15:29:12 +0700

    membuat void display

--- TugasP.cpp
+++ TugasP.cpp
@@ -40,5 +40,43 @@ public:
             else
                 REAR = REAR + 1;
         }
+        queue_array[REAR] = num;
+    }
+
+    void remove() {
+        // ccek apakah antrian kosong 
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        //cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else {
+            //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT +1;
+        }
+    }
+
+    void display() {
+
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
+
+        //cek apakah antrian kosong
+        if (FRONT_Position == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+        
+        }
+        
     }
 };
