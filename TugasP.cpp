ee259f21e68cf9c4c186a746edff6951e33595a6
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2024-05-30 14:56:45 +0700

    membuat kemungkinan memakai if

--- TugasP.cpp
+++ TugasP.cpp
@@ -20,5 +20,13 @@ public:
         int num;
         cout << "Enter a numbah: ";
         cin >> num;
+        cout << endl;
+
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return ;
+        }
+
     }
-}
+};
